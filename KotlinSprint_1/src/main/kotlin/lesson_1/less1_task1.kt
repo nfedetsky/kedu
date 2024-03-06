0609a624fa5eec30b898efcafec6d30dbc0d5115
Nicholas Fedetsky <nfedetsky@gmail.com>
2024-03-07 02:38:18 +0300

    KS-1-1 The first part of the first task

---
+++ KotlinSprint_1/src/main/kotlin/lesson_1/less1_task1.kt
@@ -0,0 +1,8 @@
+package org.example.lesson_1
+
+fun main(){
+    val productQuant : Byte = 75 //Объявление константы со значением количества заказов интернет-магазина
+    val gratitudeText : String = "Thanks for your purchase!" //Константа с текстом благодарности
+
+    println(gratitudeText)
+}
